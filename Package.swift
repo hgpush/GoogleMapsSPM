cbaa3a58ba76bdfe372510c4e31e18d3b045e094
hgpush <gitpush@google.com>
2023-01-18 11:01:52 -0800

    add ios platformn

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GoogleMapsSPM",
 		defaultLocalization: "en",
+		platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "GoogleMaps", targets: ["GoogleMaps"]),
